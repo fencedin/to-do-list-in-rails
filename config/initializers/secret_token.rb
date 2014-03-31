83bf8d72776ab0cf57ee603fc8aa21a4e2cdc406
David Adams & vincent rotter <student@epicodus.com>
2014-03-31 14:00:48 -0700

    initial commit with task model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToDo::Application.config.secret_key_base = '257f2bcf844cb1ce875cf72d678c492c306895d94d40df25428047a6d482889739057ce2210e0d29f53c7ca23897f1d8730b8eb6d5b810cb1751c84d25ed094f'
---
+++ db/migrate/20140331193005_create_task.rb
@@ -0,0 +1,9 @@
+class CreateTask < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.column :description, :string
+
+      t.timestamps
+    end
+  end
+end
